bab396e2441efe9c8b3a1d551e8f44b14b8fc2a9
Samvel <arman1993.samvel@gmail.com>
2023-05-19 12:15:49 +0400

    done

--- README.md
+++
@@ -1,3 +0,0 @@
-# speech_recognition
-<h1>Speech Recognition app</h1>
-<p>This is one-page web app, where there is one button, while holding down the button, your conversation  are received by the device, and at the end, it makes a console log in text version.</p>
--- index.js
+++ index.js
@@ -10,12 +10,50 @@ if (SpeechRecognition) {
     recognition.onresult = (event) => {
       const transcript = event.results[0][0].transcript;
       console.log("Transcript", transcript)
+
+      sendToServer(transcript)
     };
+
+    recognition.onend = () => {
+      console.log("Speech recognition ended.");
+    }
   };
 
   const stopSpeechRecognition = () => {
     recognition.stop();
     console.log("Speech recognition stopped.")
+  };
+
+  const sendToServer = (text) => {
+    const serverUrl = "My Server Url";
+    const model = "gpt-3.5-turbo";
+    const payload = {
+      text,
+      model
+    };
+
+    fetch(serverUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+    })
+      .then(response => response.json())
+      .then(data => {
+        const answer = data.answer();
+        console.log("Received answer from server:", answer);
+        playTextAsSound(answer)
+      })
+      .catch(error => {
+        console.log("Error occurred while sending text to server:", error)
+      });
+  };
+
+  const playTextAsSound = (text) => {
+    const synth = window.speechSynthesis();
+    const utterance = new SpeechSynthesisUtterance(text);
+    synth.speak(utterance);
   };
 
   const button = document.getElementById("record-button");
